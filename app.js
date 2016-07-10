866230d3042185fa03b4f2249a6f6eea8a1a4b05
Amir <Amir@mayas-macbook-air.localdomain>
2016-07-10 23:18:09 +0200

    replaced jade with handlebars and saas

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
+
+var exphbs = require('express-handlebars');
+var sassMiddleware = require('node-sass-middleware'); // ADD THIS
+
+
+var path = require('path');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
@@ -12,11 +17,27 @@ var todos = require('./routes/todos');
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+// app.set('views', path.join(__dirname, 'views'));
+// app.set('view engine', 'jade');
+
+
+// replaced with handlebars
+app.engine('hbs', exphbs({extname: '.hbs', defaultLayout: 'layout'}));
+app.set('view engine', 'hbs');
+
+//sass
+app.use (
+   sassMiddleware({
+     src: __dirname + '/saas',
+     dest: __dirname + '/public',
+    //  prefix: '/stylesheets',
+     debug: true,
+   })
+ );
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
