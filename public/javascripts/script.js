c3435560b2f04e2533a424c68a72c981a7d22dfa
Amir <baruch.amir@gmail.com>
2016-07-11 00:04:15 +0200

    Added JQuery support
    replaced listeners with jquery ones
    
    added jquery min.js link

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,15 +1,20 @@
-window.onload = function(){
-  var checkboxes = document.getElementsByTagName('input');
-  for (var i = 0; i < checkboxes.length; i++) {
-    checkboxes[i].addEventListener('click', clickHandler);
-  }
-};
-
-function clickHandler(){
-  if (this.checked) {
-    this.nextElementSibling.className = 'checked';
-    }
-  else {
-    this.nextElementSibling.className = '';
-  }
-}
+// window.onload = function(){
+//   var checkboxes = document.getElementsByTagName('input');
+//   for (var i = 0; i < checkboxes.length; i++) {
+//     checkboxes[i].addEventListener('click', clickHandler);
+//   }
+// };
+//
+// function clickHandler(){
+//   if (this.checked) {
+//     this.nextElementSibling.className = 'checked';
+//     }
+//   else {
+//     this.nextElementSibling.className = '';
+//   }
+// }
+$(function(){
+  $('input').on('click',function(){
+    $(this.nextElementSibling).toggleClass('checked');
+  });
+});
