e34c0bf8c7c6d63026c0a94ea4b871f9223a308c
Amir <baruch.amir@gmail.com>
2016-07-13 14:32:42 +0200

    Added support for browserfy,browser-sync,node-demon and mongo

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ var express = require('express');
 var favicon = require('serve-favicon');
 
 var exphbs = require('express-handlebars');
-var sassMiddleware = require('node-sass-middleware'); // ADD THIS
+var sassMiddleware = require('node-sass-middleware');
+var browserify = require('browserify-middleware');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todos');
 
 
 var path = require('path');
@@ -35,6 +38,22 @@ app.use (
    })
  );
 
+// broswerify
+app.get('/javascripts/bundle.js', browserify('./client/script.js'));
+
+// browser-sync
+if (app.get('env') == 'development') {
+  var browserSync = require('browser-sync');
+  var config = {
+    files: ["public/**/*.{js,css}", "client/*.js", "sass/**/*.scss", "views/**/*.hbs"],
+    logLevel: 'debug',
+    logSnippet: false,
+    reloadDelay: 3000,
+    reloadOnRestart: true
+  };
+  var bs = browserSync(config);
+  app.use(require('connect-browser-sync')(bs));
+}
 // uncomment after placing your favicon in /public
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
--- models/todos.js
+++ models/todos.js
@@ -1,7 +1,8 @@
-var todos = [
-  'buy milk',
-  'return book to lib',
-  'talk to Mum'
-];
-
-module.exports = todos;
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var TodosSchema = new Schema ({
+  text: {type: 'String', required: true},
+  done: {type: 'Boolean'}
+});
+var Todo = mongoose.model('Todo', TodosSchema);
+module.exports = Todo;
