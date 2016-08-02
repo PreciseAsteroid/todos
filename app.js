00afc824da02e7accca33c3bf1924069b2ba5cc6
Amir <baruch.amir@gmail.com>
2016-08-02 17:08:09 +0200

    adding logs for connecting to db2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var browserify = require('browserify-middleware');
 var mongoose = require('mongoose');
 
 mongoose.connect(process.env.MONGOLAB_URI ||'mongodb://localhost/todos');
+console.log('==== loging just the remote db', process.env.MONGOLAB_URI);
 console.log('===== Connecting to DB ... =====', process.env.MONGOLAB_URI ||'mongodb://localhost/todos');
 
 
