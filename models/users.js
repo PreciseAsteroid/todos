9caf9e25af4c830e5d6701a733ca8772fdf8d86b
Amir <baruch.amir@gmail.com>
2016-08-09 23:00:32 +0200

    added user model and /register view

---
+++ models/users.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+var Promise = require('bluebird');
+Promise.promisifyAll(mongoose);
+
+var passportLocalMongoose = require('passport-local-mongoose');
+
+var userSchema = new Schema({
+  username: String,
+  password: String
+});
+
+userSchema.plugin(passportLocalMongoose);
+
+var User = mongoose.model('User', userSchema);
+
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var passport = require('passport');
+var user = require('../models/users');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'My Todos Express', user: req.user });
+});
+
+router.get('/register',function(req, res){
+  res.render('register',{});
 });
 
 module.exports = router;
